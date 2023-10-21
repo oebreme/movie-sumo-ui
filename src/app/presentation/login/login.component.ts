e0aae1ea5c57b719be9c00fdad2b58a5d322c984
Johannes Lenk <Lenk.Johannes@gmx.de>
2023-10-21 14:31:55 +0200

    remove router import

--- src/app/presentation/login/login.component.ts
+++ src/app/presentation/login/login.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -8,10 +7,4 @@ import { Router } from "@angular/router";
 })
 export class LoginComponent {
 
-  constructor(readonly router: Router) {
-  }
-
-  log() {
-    console.log('')
-  }
 }
