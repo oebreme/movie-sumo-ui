9b481479cde46be70db7cdb2861832e9075e793d
Johannes Lenk <Lenk.Johannes@gmx.de>
2023-12-08 12:10:19 +0100

    refactor test-suites to use testing library

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,21 +1,15 @@
-import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { render } from "@testing-library/angular";
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+
   beforeEach(async() => {
-    await render(AppComponent);
+    const renderResult = await render(AppComponent);
+    component = renderResult.fixture.componentInstance;
   });
 
   test('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  test(`should have as title 'movie-sumo-ui'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('movie-sumo-ui');
+    expect(component).toBeTruthy();
   });
 });
