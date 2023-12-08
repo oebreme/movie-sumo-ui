4f26f4b2758ba90dd03fd1c8b2d5188e1730db26
Johannes Lenk <Lenk.Johannes@gmx.de>
2023-12-08 10:14:50 +0100

    rm auto-generated, unneeded test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('movie-sumo-ui');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('movie-sumo-ui app is running!');
-  });
 });
