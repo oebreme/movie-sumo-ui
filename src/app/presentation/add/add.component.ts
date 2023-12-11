378cb3a68f6bd46f42d8b1750c589cc042ef5591
Johannes Lenk <Lenk.Johannes@gmx.de>
2023-12-11 01:17:07 +0100

    clean-up add-component

--- src/app/presentation/add/add.component.ts
+++ src/app/presentation/add/add.component.ts
@@ -1,50 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { debounceTime, Subject } from "rxjs";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
   styleUrls: ['./add.component.scss']
 })
-export class AddComponent implements OnInit {
-
-  userInput: string = '';
-  inputSubject: Subject<void> = new Subject<void>();
-  list: any;
-  isLoading: boolean = false;
-
-  constructor(private readonly http: HttpClient) {
-  }
-
-  ngOnInit(): void {
-    console.log('ngOnInit called at ~~~ ' + new Date().toLocaleString());
-    this.inputSubject.subscribe(() => {
-      this.isLoading = true
-    });
-    this.inputSubject.pipe(debounceTime(500))
-      .subscribe(() => {
-      console.log('called API ~~~ ' + new Date().toLocaleString());
-      this.callTmdb();
-    })
-  }
-
-  buildImgUrl(e: string): string {
-    return "https://image.tmdb.org/t/p/w500/" + e;
-  }
-
-  callTmdb(): void {
-    let headers = new HttpHeaders();
-    headers = headers.set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZmRjMWEzZDM1MTIyYWE3MjgyZmVlNTlmNmQwOTAzNiIsInN1YiI6IjY1M2NkOTVmNzE5YWViMDBmZTNjNzY4YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QK-5wspCxMBkDCnQj5elVym5CYN_rOBDg_w_-UYB2HY')
-    headers = headers.append('Content-Type', 'application/json');
-
-    this.http.get('https://api.themoviedb.org/3/search/movie?query=' + this.userInput + '&include_adult=false&language=en-US&page=1',
-      {headers: headers}, ).subscribe((data) => {
-        this.list = data;
-      this.isLoading = false;
-      console.log(data);
-    });
-  }
-
-
+export class AddComponent {
 }
