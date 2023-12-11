29dffc7013b23ed272f3484483b16384ee50045f
Johannes Lenk <Lenk.Johannes@gmx.de>
2023-12-11 01:13:25 +0100

    declare services in shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchBarComponent } from './presentation/search-bar/search-bar.component';
+import { FormsModule } from "@angular/forms";
+import { SearchService } from "./delivery/search.service";
+import { SearchResultMapper } from "./data/search-result-mapper.service";
 
 @NgModule({
   declarations: [
     SearchBarComponent
   ],
+  exports: [
+    SearchBarComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
+  ],
+  providers: [
+    SearchService,
+    SearchResultMapper
   ]
 })
 export class SharedModule { }
