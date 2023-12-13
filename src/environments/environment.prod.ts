9af41513807a0847f0a63e7f46d9d9550a0e78c3
Johannes Lenk <Lenk.Johannes@gmx.de>
2023-12-14 00:22:09 +0100

    rely on environment-fields to receive authToken & further api-config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  tmdbDefaultLanguage: 'de',
+  tmdbToken: 'QmVhcmVyIGV5SmhiR2NpT2lKSVV6STFOaUo5LmV5SmhkV1FpT2lJell6RXdNemxqTURNNU5qbGhaRGczTkdNM01tTmxNbU0zTXpjMlpUQXhPQ0lzSW5OMVlpSTZJalkxTTJOa09UVm1OekU1WVdWaU1EQm1aVE5qTnpZNFlTSXNJbk5qYjNCbGN5STZXeUpoY0dsZmNtVmhaQ0pkTENKMlpYSnphVzl1SWpveGZRLmhCZ1dyampTaVRqZVBKNTk1WlBuZk1EdXh2SWZaTjVvaF9QZ1Bub3RwS0E='
 };
