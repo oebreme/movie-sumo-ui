d4bd38182c650612c30748dd3edd73b92701a6dd
Johannes Lenk <Lenk.Johannes@gmx.de>
2023-12-14 14:52:23 +0100

    start impl dumb component for displaying a search-result

---
+++ src/app/shared/presentation/search-result/search-result.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-result',
+  templateUrl: './search-result.component.html',
+  styleUrls: ['./search-result.component.scss']
+})
+export class SearchResultComponent {
+
+  // TODO:
+  //  - Die Idee wäre: diese Comp soll über Input ein objekt erhalten mit allen notwendig ingos :D und einfach doof darstellen
+  //  - Dazu muss es aber noch ein Output geben, WEIL man einen Button hat, der den Film zu einer Liste hinzufügen soll
+
+}
