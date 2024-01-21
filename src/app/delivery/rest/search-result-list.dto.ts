92cdfe0847a339c786f90f859a247e5a8e2f134d
Johannes Lenk <Lenk.Johannes@gmx.de>
2024-01-22 00:33:02 +0100

    define types based on generated-dtos from contract

---
+++ src/app/delivery/rest/search-result-list.dto.ts
@@ -0,0 +1,3 @@
+import { SearchResultList as genSearchResultList } from 'src/app/delivery/rest/generated/model/searchResultList';
+
+export type SearchResultListDto = genSearchResultList;
---
+++ src/app/delivery/rest/search-result.ts
@@ -0,0 +1,3 @@
+import { SearchResult as genSearchResult } from 'src/app/delivery/rest/generated/model/searchResult';
+
+export type SearchResultDto = genSearchResult;
