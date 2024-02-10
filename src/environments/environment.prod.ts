e2fb463d2bba6783eebd87fe91acb14f10a90dc5
Johannes Lenk <Lenk.Johannes@gmx.de>
2024-02-10 01:00:42 +0100

    add gateway-config & app-config token

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { authConfig } from "../app/config/auth.config";
 
 export const environment = {
   production: true,
-  tmdbDefaultLanguage: 'de',
-  tmdbToken: 'QmVhcmVyIGV5SmhiR2NpT2lKSVV6STFOaUo5LmV5SmhkV1FpT2lJell6RXdNemxqTURNNU5qbGhaRGczTkdNM01tTmxNbU0zTXpjMlpUQXhPQ0lzSW5OMVlpSTZJalkxTTJOa09UVm1OekU1WVdWaU1EQm1aVE5qTnpZNFlTSXNJbk5qYjNCbGN5STZXeUpoY0dsZmNtVmhaQ0pkTENKMlpYSnphVzl1SWpveGZRLmhCZ1dyampTaVRqZVBKNTk1WlBuZk1EdXh2SWZaTjVvaF9QZ1Bub3RwS0E=',
   authConfig: authConfig,
+  defaultLanguage: 'de',
+  movieSumoGatewayUrl: 'https://movie-sumo-gateway.oebreme.dev'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ import { authConfig } from "../app/config/auth.config";
 
 export const environment = {
   production: false,
-  tmdbDefaultLanguage: 'de',
-  tmdbToken: 'QmVhcmVyIGV5SmhiR2NpT2lKSVV6STFOaUo5LmV5SmhkV1FpT2lJell6RXdNemxqTURNNU5qbGhaRGczTkdNM01tTmxNbU0zTXpjMlpUQXhPQ0lzSW5OMVlpSTZJalkxTTJOa09UVm1OekU1WVdWaU1EQm1aVE5qTnpZNFlTSXNJbk5qYjNCbGN5STZXeUpoY0dsZmNtVmhaQ0pkTENKMlpYSnphVzl1SWpveGZRLmhCZ1dyampTaVRqZVBKNTk1WlBuZk1EdXh2SWZaTjVvaF9QZ1Bub3RwS0E=',
   authConfig: authConfig,
+  defaultLanguage: 'de',
+  movieSumoGatewayUrl: 'http://localhost:8081'
 };
