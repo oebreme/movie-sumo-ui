23a8aa3697d20206ead391b3e52e1aafdf07c886
Johannes Lenk <Lenk.Johannes@gmx.de>
2024-03-19 23:56:27 +0100

    use v3 api models

--- src/app/delivery/rest/movie-provider.dto.ts
+++ src/app/delivery/rest/movie-provider.dto.ts
@@ -1,3 +1,3 @@
-import { StreamProvider as genMovieDetails } from 'src/app/delivery/rest/generated/model/streamProvider';
+import { MovieProvider as genMovieProvider } from 'src/app/delivery/rest/generated/model/movieProvider';
 
-export type StreamProviderDto = genMovieDetails;
+export type MovieProviderDto = genMovieProvider;
