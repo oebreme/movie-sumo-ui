7d996a9174fe1fa8cfcbc11492dde53b54aed428
Johannes Lenk <Lenk.Johannes@gmx.de>
2024-03-19 23:55:52 +0100

    movie-preview moved to own type-declaration

---
+++ src/app/delivery/rest/movie-preview.dto.ts
@@ -0,0 +1,3 @@
+import { MoviePreview as genMoviePreview } from 'src/app/delivery/rest/generated/model/moviePreview';
+
+export type MoviePreviewDto = genMoviePreview;
--- src/app/delivery/rest/movie.dto.ts
+++ src/app/delivery/rest/movie.dto.ts
@@ -1,5 +1,3 @@
 import { Movie as genMovie } from 'src/app/delivery/rest/generated/model/movie';
 
 export type MovieDto = genMovie;
-
-export type MoviePreviewDto = Omit<genMovie, 'details'>;
