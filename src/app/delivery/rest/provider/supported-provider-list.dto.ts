2a0e2e9d35c8dd1de1f11a803db5266928b6c303
Johannes Lenk <Lenk.Johannes@gmx.de>
2024-03-22 23:43:15 +0100

    more dtos

---
+++ src/app/delivery/rest/provider/supported-provider-list.dto.ts
@@ -0,0 +1,3 @@
+import { SupportedProviderList as genSupportedProviderList } from "../generated/model/supported-provider-list.api";
+
+export type SupportedProviderListDto = genSupportedProviderList;
---
+++ src/app/delivery/rest/provider/supported-provider.dto.ts
@@ -0,0 +1,3 @@
+import { SupportedProvider as genSupportedProvider } from "../generated/model/supported-provider.api";
+
+export type SupportedProviderDto = genSupportedProvider;
