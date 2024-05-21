3ca03967fd195a8dfa2f13a784dc067563f99389
Johannes Lenk <Lenk.Johannes@gmx.de>
2024-05-22 00:30:01 +0200

    add custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#B819F0",
+          "secondary": "#C43364",
+          "accent": "#F69159",
+          "neutral": "#fff",
+          "base-100": "#0B0C10",
+          "info": "#00B5FF",
+          "success": "#00A96E",
+          "warning": "#FFBE00",
+          "error": "#FF5861",
+        },
+      }
+    ]
+  },
   content: [
     "./src/**/*.{html,ts}"
   ],
