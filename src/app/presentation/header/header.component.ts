8a15b40e605589c1c49bd436e52a84c771a7f31f
Johannes Lenk <Lenk.Johannes@gmx.de>
2024-06-01 14:06:41 +0200

    change component-prefix to 'moviesumo'

--- src/app/presentation/header/header.component.ts
+++ src/app/presentation/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
+  selector: 'moviesumo-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
