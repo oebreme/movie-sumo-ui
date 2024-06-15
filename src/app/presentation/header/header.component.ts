d4119f1c75d0505e19d16eb4386519adfedf6d43
Johannes Lenk <Lenk.Johannes@gmx.de>
2024-06-15 11:19:19 +0200

    how to combine angular palette with existing daiysui theme? ( •̀ ω •́ )✧

--- src/app/presentation/header/header.component.ts
+++ src/app/presentation/header/header.component.ts
@@ -1,13 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit, viewChild } from '@angular/core';
 import { AsyncPipe, NgOptimizedImage, NgTemplateOutlet } from '@angular/common';
 import { AuthService, User } from '@auth0/auth0-angular';
 import { take } from 'rxjs';
 import { RouterLink } from '@angular/router';
+import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'moviesumo-header',
   standalone: true,
-  imports: [NgTemplateOutlet, AsyncPipe, NgOptimizedImage, RouterLink],
+  imports: [
+    NgTemplateOutlet,
+    AsyncPipe,
+    NgOptimizedImage,
+    RouterLink,
+    MatMenu,
+    MatMenuItem,
+    MatMenuTrigger,
+  ],
   templateUrl: './header.component.html',
 })
 export class HeaderComponent implements OnInit {
@@ -19,5 +29,13 @@ export class HeaderComponent implements OnInit {
     this.auth.user$.pipe(take(1)).subscribe((user) => {
       this.user = user;
     });
+  }
+
+  readonly menuTrigger = viewChild.required(MatMenuTrigger);
+
+  readonly dialog = inject(MatDialog);
+
+  openDialog() {
+    console.log('haiiiiii');
   }
 }
