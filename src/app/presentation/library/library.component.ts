acc347e3ca97cc29d974c9e3700111b2c02672d5
[oebreme] <Lenk.Johannes@schueler.6072-fuerth.de>
2024-11-02 23:42:02 +0100

    start implementing watchlist & favorites cards

--- src/app/presentation/library/library.component.ts
+++ src/app/presentation/library/library.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { SearchBarComponent } from "../search/search-bar/search-bar.component";
 import { SearchResultsComponent } from "../search/search-results/search-results.component";
+import { DatePipe } from "@angular/common";
 
 @Component({
   selector: 'moviesumo-library',
   standalone: true,
   imports: [
     SearchBarComponent,
-    SearchResultsComponent
+    SearchResultsComponent,
+    DatePipe
   ],
   templateUrl: './library.component.html'
 })
