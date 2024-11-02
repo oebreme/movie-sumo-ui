bcad0a0c39667f434234f3bba542eebfc220355f
[oebreme] <Lenk.Johannes@schueler.6072-fuerth.de>
2024-11-02 14:01:41 +0100

    add new search-bar

---
+++ src/app/presentation/search/search-bar/search-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'moviesumo-search-bar',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-bar.component.html'
+})
+export class SearchBarComponent {
+
+}
--- src/app/presentation/search/search.component.ts
+++ src/app/presentation/search/search.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { TrendingComponent } from "../home/trending/trending.component";
 import { UpcomingComponent } from "../home/upcoming/upcoming.component";
+import { SearchBarComponent } from "./search-bar/search-bar.component";
 
 @Component({
   selector: 'moviesumo-search',
   standalone: true,
   imports: [
     TrendingComponent,
-    UpcomingComponent
+    UpcomingComponent,
+    SearchBarComponent
   ],
   templateUrl: './search.component.html'
 })
